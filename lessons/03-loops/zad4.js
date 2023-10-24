c5ac99b15d0997c9335d38c1f9d16191c31afab8
Joseph Mac <josephmac@interia.pl>
2023-10-24 14:19:35 +0200

    for, while, zad4

---
+++ lessons/03-loops/zad4.js
@@ -0,0 +1,11 @@
+
+const n = 50;
+let line = "";
+
+for (let i = 1; i <= n; i++) {
+    let line = "";
+    for(let j = 1; j <= i; j++) {
+        line = line + " " + j;
+    }
+    console.log(line);
+}
