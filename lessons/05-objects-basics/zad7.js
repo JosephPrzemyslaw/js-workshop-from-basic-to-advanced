575765151d41e31b80ca5d562f6fee6c3da1e249
Joseph Mac <josephmac@interia.pl>
2023-10-25 15:54:50 +0200

    Functions

---
+++ lessons/05-objects-basics/zad7.js
@@ -0,0 +1,24 @@
+function Ball(color, size) {
+    this.color = color;
+    this.size = size;
+    this.type = 'regular';
+    this.pressure = '4.5';
+}
+ 
+Ball.prototype.showDescr = function(){
+    console.log(`This is a ${this.type} ball\n` +
+    `Pressure value: ${this.pressure}\n` +
+    `Color: ${this.color}\n` + 
+    `Size: ${this.size}\n`);
+}
+ 
+ const ball = new Ball('yellow', '5');
+ const ballDescr = ball.showDescr();
+ 
+ const anotherBall = new Ball('green', '6');
+ const anotherBallDescr = ball.showDescr();
+
+ const yetAnotherBall = new Ball('black', '6');
+ const yetAnotherBallDescr = ball.showDescr();
+
+ debugger;
