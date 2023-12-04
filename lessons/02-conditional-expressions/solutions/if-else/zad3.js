31c9f11273135814ae7cc51c7150d8754c175d29
Joseph Mac <josephmac@interia.pl>
2023-12-04 13:59:06 +0100

    if - else, zad3

---
+++ lessons/02-conditional-expressions/solutions/if-else/zad3.js
@@ -0,0 +1,18 @@
+// sample data
+const a = -12;
+const b = -120;
+const c = -1200;
+
+// check if a < b < c
+if (a < b && b < c) {
+    console.log("tak, a < b < c");
+} else {
+    console.log("nie, a < b < c");
+}
+
+// check ic a > b > c
+if (a > b && b > c) {
+    console.log("tak, a > b > c");
+} else {
+    console.log("nie, a > b > c");
+}
