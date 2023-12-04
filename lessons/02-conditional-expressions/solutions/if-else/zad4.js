8ed3d5c3cdc962178d58e035eb6811409a4b0a78
Joseph Mac <josephmac@interia.pl>
2023-12-04 14:14:54 +0100

    If else, zad4

---
+++ lessons/02-conditional-expressions/solutions/if-else/zad4.js
@@ -0,0 +1,12 @@
+// sample data
+const a = 3;
+const b = 5;
+const c = 7;
+
+if (a + b > c && a + c > b && b + c > a &&
+    a > 0 && b >0 && c > 0) {
+    console.log("tak");
+} else {
+    console.log("nie");
+}
+
